1e32cbaa75789b3eb9608658fb16ccbbbe174008
topi0247 <karubona.ra247@gmail.com>
2025-03-16 14:07:56 +0900

    fix: 文字数バリデーションが表示されるよう修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ApplicationRecord
   validates :presentation_category, presence: true
   validates :target_category, presence: true
 
-  enum presentation_category: { lt: 0, proposal: 1, invitation: 2 }, _default: 1
-  enum target_category: { beginner: 0, basic: 1, developer: 2 , alumni: 3 }, _default: 1
+  enum presentation_category: { lt: 0, proposal: 1, invitation: 2 }, _default: 0
+  enum target_category: { beginner: 0, basic: 1, developer: 2 , alumni: 3 }, _default: 0
 end
