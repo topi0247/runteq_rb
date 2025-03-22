bdbfbcf8a8724be880c47dd85aa41ea8fbf0aae3
sumisumi2000 <sumio.vantan308@gmail.com>
2025-03-22 19:12:49 +0900

    remove: マイページ関連を削除

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,29 +22,4 @@ class Admin::UsersController < Admin::ApplicationController
   def user_params
     params.require(:user).permit(:id, :role)
   end
-
-  def show
-    @user = current_user
-    @posts = current_user.posts
-  end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-
-    if @user.update(user_params)
-      redirect_to admin_users_path, notice: "ユーザー情報を更新しました"
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :x_id, :social_portfolio_url, :image_url)
-  end
 end
