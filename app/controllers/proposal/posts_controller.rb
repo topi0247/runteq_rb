c686d3048c9469d2fc935825dfc3905ed3e279f8
yuekami <yuekami7@gmail.com>
2025-03-22 17:04:37 +0900

    fix: プロポーザル詳細画面で応募が見つからなかった場合の遷移先を修正

--- app/controllers/proposal/posts_controller.rb
+++ app/controllers/proposal/posts_controller.rb
@@ -2,7 +2,7 @@ class Proposal::PostsController < Proposal::ApplicationController
   def show
     @post = current_user.posts.find_by(id: params[:id])
     if @post.nil?
-      redirect_to proposal_posts_path, alert: 'プロポーザルが見つかりません。'
+      redirect_to proposal_root_path, alert: 'プロポーザルが見つかりません。'
     end
   end
 
