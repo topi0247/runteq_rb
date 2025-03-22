38364101fccc6b1f75be46dd3e23791929211758
sumisumi2000 <sumio.vantan308@gmail.com>
2025-03-22 16:00:05 +0900

    feat: 管理者用の投稿詳細ページを作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,8 @@
 class Admin::PostsController < Admin::ApplicationController
-  def index
-    @posts = Post.includes(:user).all
+  def show
+    @post = current_user.posts.find_by(id: params[:id])
+    if @post.nil?
+      redirect_to proposal_posts_path, alert: 'プロポーザルが見つかりません。'
+    end
   end
 end
