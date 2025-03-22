bf4c4254b4b4a3029cad25788c892d4c7df81a9e
topi0247 <karubona.ra247@gmail.com>
2025-03-22 17:37:47 +0900

    fix: フラッシュメッセージが表示されるよう修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,10 +4,13 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
   def role_change
-    logger.debug user_params
     @user = User.find(user_params[:id])
     if @user.update(role: user_params[:role])
-      flash.now[:success] = "ロールを変更しました"
+      if current_user.admin?
+        flash.now[:success] = "ロールが変更されました。"
+      else
+        redirect_to root_path, alert: "ロールが変更されました。アクセス権限がありません。", status: :see_other
+      end
     else
       flash.now[:error] = "ロールの変更に失敗しました"
       render :index
--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,5 +1,2 @@
 module Admin::ApplicationHelper
-  def turbo_update_flash
-    turbo_stream.update "flash", partial: "shared/flash_message"
-  end
 end
