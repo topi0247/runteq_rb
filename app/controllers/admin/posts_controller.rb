b9abc2da1258b4ffd2361cc7408afc626a39a645
田畑友作 <kuppa0713@icloud.com>
2025-03-26 09:29:46 +0900

    feat:登壇枠と0ターゲット層を絞り込む機能を追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,18 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
-    @posts = Post.includes(:user).order(created_at: :desc)
+    @posts = Post.includes(:user).all
+    if params[:presentation_category].present?
+      @posts = @posts.where(presentation_category: params[:presentation_category])
+    end
+    if params[:target_category].present?
+      @posts = @posts.where(target_category: params[:target_category])
+    end
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    if @post.nil?
+      redirect_to admin_posts_path, alert: '応募が見つかりません。'
+    end
   end
 end
