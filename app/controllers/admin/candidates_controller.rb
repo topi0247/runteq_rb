8a95fb54d756c773887a0b5bcf8bad17d84afde8
yuekami <yuekami7@gmail.com>
2025-03-29 02:02:05 +0900

    fix: 候補ボタンをturboに変更

--- app/controllers/admin/candidates_controller.rb
+++ app/controllers/admin/candidates_controller.rb
@@ -1,13 +1,11 @@
 class Admin::CandidatesController < Admin::ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.candidate(post)
-    redirect_to request.url, flash: { success: "候補にしました" }
+    @post = Post.find(params[:post_id])
+    current_user.candidate(@post)
   end
 
   def destroy
-    post = current_user.candidates.find(params[:id]).post
-    current_user.uncandidate(post)
-    redirect_to request.url, flash: { success: "候補を削除しました" }, status: :see_other
+    @post = current_user.candidates.find(params[:id]).post
+    current_user.uncandidate(@post)
   end
 end
