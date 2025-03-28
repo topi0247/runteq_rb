09f9d1f96a67536bccb1aacc340d682d24640d28
yuekami <yuekami7@gmail.com>
2025-03-28 22:21:27 +0900

    feat: Candidatesコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :posts, only: %i[index show]
-    root to: "users#index"
-    resources :posts, only: [:index]
+    patch "role_change" => "users#role_change"
+    resources :candidates, only: %i[create destroy]
   end
 end
