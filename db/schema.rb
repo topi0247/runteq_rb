95b436ce6c44109ca04f2dcb36c8354524cb6be7
topi0247 <karubona.ra247@gmail.com>
2025-04-13 11:37:22 +0900

    feat: プレゼン順のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_29_145809) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_13_023526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_29_145809) do
     t.integer "presentation_category", null: false
     t.integer "target_category", null: false
     t.integer "selection", default: 0
+    t.integer "presentation_position"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -47,6 +48,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_29_145809) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.string "comment"
   end
 
   add_foreign_key "candidates", "posts"
