74989a145437a268dd072ff072160aac18691152
石井悠香 <i.yu-ka@ishiiyuukounoMacBook-Air.local>
2025-04-21 14:04:45 +0900

    fix:時間変更箇所なし/登壇者修正

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -3,6 +3,8 @@ class TimelinesController < ApplicationController
     posts = Post.includes(:user).where(selection: Post.selections[:adoption]).order(presentation_position: :asc)
     @invitations = posts.where(presentation_category: Post.presentation_categories[:invitation])
     @lts = posts.where(presentation_category: Post.presentation_categories[:lt])
+                # .where.not(user_id:8 )
+                .where.not(users: { name: "やま さかな" })
     @proposals = posts.where(presentation_category: Post.presentation_categories[:proposal])
     @admins = User.where(role: User.roles[:admin]).order(created_at: :asc)
   end
